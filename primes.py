81c3d9a6eca26ef2913afd2b818bc4dcba4169ea
Mohammed Tahsin Kazi <mohammed.kazi@kcl.ac.uk>
2023-10-22 23:50:26 +0100

    Complete primes function

--- primes.py
+++ primes.py
@@ -2,5 +2,19 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
+    if number_of_primes <= 0:
+        raise ValueError("Enter a number bigger than 0")
+    list = [2]
+    number = 2
+    while len(list) < number_of_primes:
+        if number not in list:
+            indivisable = True
+            for prime in list:
+                if number % prime == 0:
+                    indivisable = False
+            if indivisable:
+                list.append(number)
+        number += 1
     return list
+
+print(primes(10))
